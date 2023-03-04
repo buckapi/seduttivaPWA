8cec9db9a5697aa2e3763271e70ffd4748242237
buckapi <buckapis@gmail.com>
2023-03-04 14:48:19 -0400

    newdesing

--- src/app/services/script.store.ts
+++ src/app/services/script.store.ts
@@ -4,15 +4,19 @@
     src: string;
 }  
 export const ScriptStore: Scripts[] = [
-  {name: 'bootstrap', src: './assets/assetsCatal/libs/bootstrap/js/bootstrap.bundle.min.js'},
-  {name: 'tiny-slider', src: './assets/assetsCatal//libs/tiny-slider/min/tiny-slider.js'},
-  {name: 'feather', src: './assets/assetsCatal/libs/feather-icons/feather.min.js'},
-  {name: 'plugins', src: './assets/assetsCatal/js/plugins.init.js'},
-  {name: 'app', src: './assets/assetsCatal/js/app.js'}
+  {name: 'jquery', src: './assets/assetsSeduttiva/plugins/jquery-1.12.4.min.js'},
+  {name: 'selectize', src: './assets/assetsSeduttiva/plugins/selectize/dist/js/standalone/selectize.min.js'},
+  {name: 'plugins', src: './assets/assetsSeduttiva/plugins/plugins.min.js'}, 
+  {name: 'main', src: './assets/assetsSeduttiva/js/main.js'} 
+
 ];
 
 
-
+/* {name: 'bootstrap', src: './assets/assetsCatal/libs/bootstrap/js/bootstrap.bundle.min.js'},
+{name: 'tiny-slider', src: './assets/assetsCatal//libs/tiny-slider/min/tiny-slider.js'},
+{name: 'feather', src: './assets/assetsCatal/libs/feather-icons/feather.min.js'},
+{name: 'plugins', src: './assets/assetsCatal/js/plugins.init.js'},
+{name: 'app', src: './assets/assetsCatal/js/app.js'} */
 
 
 
